4a43563abe976a8087d6b998d4e8facd7d6d5fbf
Christian Neumüller <cn00@gmx.at>
2014-12-20 23:59:48 +0100

    ARM: Exynos: Call cpu_pm_enter/exit in Low Power States.
    In low power cpuidle states, the FPU registers get corrupted. By calling
    cpu_pm_enter, the VFP module gets notified so that it saves the FPU state and
    restores it from RAM the next time a thread uses it.
    
    See http://forum.xda-developers.com/showthread.php?p=57637134.
    
    Change-Id: Id3a455b9c7c3ff5e45e9c0cdb04719bb370b87e9

--- arch/arm/mach-exynos/cpuidle-exynos4.c
+++ arch/arm/mach-exynos/cpuidle-exynos4.c
@@ -10,6 +10,7 @@
 
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/cpu_pm.h>
 #include <linux/cpuidle.h>
 #include <linux/io.h>
 #include <linux/suspend.h>
@@ -939,6 +940,7 @@ static int exynos4_enter_lowpower(struct cpuidle_device *dev,
 	if (!enter_mode)
 		return exynos4_enter_idle(dev, new_state);
 	else {
+		cpu_pm_enter();
 #ifdef CONFIG_CORESIGHT_ETM
 		etm_disable(0);
 #endif
@@ -949,6 +951,7 @@ static int exynos4_enter_lowpower(struct cpuidle_device *dev,
 #ifdef CONFIG_CORESIGHT_ETM
 		etm_enable(0);
 #endif
+		cpu_pm_exit();
 	}
 
 	return ret;
