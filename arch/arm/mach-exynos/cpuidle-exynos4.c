82b2871092dc3cd05855de0cf80d7194ac3f7dd9
Clément Guérin <geecko.dev@free.fr>
2015-01-02 18:44:02 +0100

    ARM: Exynos: Call PM notifiers w/ IRQs disabled.
    The comments at cpu_pm_enter/exit indicate that these functions must be called
    with interrupts disabled, so move the calls between
    local_irq_disable/local_irq_enable.

--- arch/arm/mach-exynos/cpuidle-exynos4.c
+++ arch/arm/mach-exynos/cpuidle-exynos4.c
@@ -585,6 +585,8 @@ static int exynos4_enter_core0_aftr(struct cpuidle_device *dev,
 
 	local_irq_disable();
 
+	cpu_pm_enter();
+
 	if (log_en)
 		pr_info("+++aftr\n");
 
@@ -647,6 +649,8 @@ early_wakeup:
 	if (log_en)
 		pr_info("---aftr\n");
 
+	cpu_pm_exit();
+
 	local_irq_enable();
 	idle_time = (after.tv_sec - before.tv_sec) * USEC_PER_SEC +
 		    (after.tv_usec - before.tv_usec);
@@ -679,6 +683,8 @@ static int exynos4_enter_core0_lpa(struct cpuidle_device *dev,
 	bt_uart_rts_ctrl(1);
 #endif
 	local_irq_disable();
+
+	cpu_pm_enter();
 
 #ifdef CONFIG_INTERNAL_MODEM_IF
 	gpio_set_value(GPIO_PDA_ACTIVE, 0);
@@ -779,6 +785,8 @@ early_wakeup:
 #ifdef CONFIG_INTERNAL_MODEM_IF
 	gpio_set_value(GPIO_PDA_ACTIVE, 1);
 #endif
+
+	cpu_pm_exit();
 
 	local_irq_enable();
 	idle_time = (after.tv_sec - before.tv_sec) * USEC_PER_SEC +
@@ -940,7 +948,6 @@ static int exynos4_enter_lowpower(struct cpuidle_device *dev,
 	if (!enter_mode)
 		return exynos4_enter_idle(dev, new_state);
 	else {
-		cpu_pm_enter();
 #ifdef CONFIG_CORESIGHT_ETM
 		etm_disable(0);
 #endif
@@ -951,7 +958,6 @@ static int exynos4_enter_lowpower(struct cpuidle_device *dev,
 #ifdef CONFIG_CORESIGHT_ETM
 		etm_enable(0);
 #endif
-		cpu_pm_exit();
 	}
 
 	return ret;
